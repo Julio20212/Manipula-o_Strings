10a3464d47203466e81f3a25d908440b302381e3
juliomoraes <juliomoraes1000@gmail.com>
2025-04-08 16:26:21 -0300

    manipulacaostrings

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Atividade 1
+//function enviar() {
+    //const opa = document.getElementById('opa').value;
+  
+    //let textoVerificado = opa.startsWith("Ola");
+    //console.log(textoVerificado);
+  //}
+
+
+//Atividade 2
+
+//function verificar() {
+    //const palavra = document.getElementById('palavra').value;
+  
+    //let terminaComCao = palavra.endsWith("ção");
+    //console.log(terminaComCao);
+  //}
+  
+//Atividade 3
+
+function mudaCor() {
+    const cor = document.getElementById('caixa');
+    cor.style.backgroundColor = "green";
+}
