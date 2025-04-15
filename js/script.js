25179f304f77b7beb0099a23432754ef618bf0b8
julio20212 <162452155+Julio20212@users.noreply.github.com>
2025-04-15 15:19:00 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,55 @@
 // Atividade 1
 //function enviar() {
-    //const opa = document.getElementById('opa').value;
-  
-    //let textoVerificado = opa.startsWith("Ola");
-    //console.log(textoVerificado);
-  //}
+//const opa = document.getElementById('opa').value;
+
+//if (opa.startsWith("Ola")){
+//  alert("Olá mundo");
+//  }
+// else {
+//  alert("A frase não tem ola")
+//  }
+//
+//}
 
 
 //Atividade 2
 
 //function verificar() {
-    //const palavra = document.getElementById('palavra').value;
-  
-    //let terminaComCao = palavra.endsWith("ção");
-    //console.log(terminaComCao);
-  //}
-  
+//const palavra = document.getElementById('palavra').value;
+
+//let terminaComCao = palavra.endsWith("ção");
+//console.log(terminaComCao);
+//}
+
 //Atividade 3
 
-function mudaCor() {
-    const cor = document.getElementById('caixa');
-    cor.style.backgroundColor = "green";
+//function mudaCor() {
+//const cor = document.getElementById('caixa');
+//cor.style.backgroundColor = "green";
+//}
+
+//Atividade 4
+
+//function alternar() {
+    //const desaparecer = document.getElementById('desaparecer');
+    //if(desaparecer.style.display === "block"){
+        //desaparecer.style.display = "none";
+    //}
+    //else {
+       // desaparecer.style.display = "block";
+    //}
+//}
+
+//Atividade 5
+function ativ5() {
+    const aumentar = document.getElementById('aumentar');
+
+    let tamanho = aumentar.style.fontSize;
+
+    if (tamanho === ""){
+        tamanho = "16px";
+    }
+
+    let tamanhoNumero = parseInt(tamanho);
+    aumentar.style.fontSize = (tamanhoNumero + 2) + "px";
 }
